fb6d40f86b937a3549500e71a7e3ce8cb77f4e3b
Rindula <rindola@t-online.de>
2017-09-27 20:07:54 +0200

    Bücherliste

---
+++ listen/buecherliste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bücherliste</title>
+</head>
+<body>
+    <table>
+            <tr><th>Author</th><th>Name</th><th>Genre</th></tr>
+        <?php
+        $conn = new mysqli("25.83.12.108", "root", "SiSal2002", "gfs");
+        $r = $conn->query("SELECT z.bez, k.genre, z.author FROM bestand AS z INNER JOIN beschreibung AS b ON b.id = z.id INNER JOIN genre AS k ON b.genre = k.id");
+        echo $conn->error;
+        while ($a = $r->fetch_array()) {
+            echo "<tr><td>".$a[2]."</td><td>".$a[0]."</td><td>".$a[1]."</td></tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
