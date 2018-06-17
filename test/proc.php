cd287def47ec20ece41b6a77a818c8387276dfad
Rindula <contact@rindula.de>
2018-06-17 18:41:13 +0200

    Debug

--- test/proc.php
+++ test/proc.php
@@ -3,8 +3,14 @@
 $m = $_GET["m"];
 $r = $_GET["r"];
 
+echo $m;
+echo $r;
+
 $ma = explode(",", $m);
 $ra = explode(",", $r);
+
+print_r($ma);
+print_r($ra);
 
 for ($i=0; $i < count($ma); $i++) { 
     $out[] = ($ma[i] - $ra[i]);
